ba7ac88f4c6e8ff0d7fa4f1afc13b7176fd54cfc
M-Abeer <102969595+M-Abeer@users.noreply.github.com>
2024-04-18 00:58:11 +0500

    snapgram

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -8,9 +8,14 @@ const AuthLayout = () => {
         <Navigate to="/" />
       ) : (
         <>
-          <section>
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
+          <img
+            src="/assets/images/side-img.svg"
+            alt="logo"
+            className="hidden xl:block w-1/2 h-screen object-cover bg-no-repeat"
+          />
         </>
       )}
     </>
