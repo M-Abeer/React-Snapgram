a851af2b6d46de31365795d660a8923bb396d085
M-Abeer <102969595+M-Abeer@users.noreply.github.com>
2024-04-18 21:24:46 +0500

    snapgram

--- src/lib/appwrite/config.ts
+++ src/lib/appwrite/config.ts
@@ -1,1 +1,12 @@
-import {Client}
+import { Client, Account, Databases, Storage, Avatars } from "appwrite";
+import { url } from "inspector";
+export const appwriteConfig = {
+  projectId: "6621397bb05e5908c90e",
+  url: "https://cloud.appwrite.io/v1",
+};
+
+export const client = new Client();
+export const account = new Account(client);
+export const databases = new Databases(client);
+export const storage = new Storage(client);
+export const avatars = new Avatars(client);
